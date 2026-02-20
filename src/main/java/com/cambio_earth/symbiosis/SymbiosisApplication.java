abdee175823c7fb957d0ffac28fac49bf41a2a56
Alyoshka <alyoshkamyshko@gmail.com>
2026-02-19 18:39:25 -0800

    chore(init): initialize Springboot app

---
+++ src/main/java/com/cambio_earth/symbiosis/SymbiosisApplication.java
@@ -0,0 +1,13 @@
+package com.cambio_earth.symbiosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbiosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbiosisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cambio_earth/symbiosis/SymbiosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambio_earth.symbiosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbiosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
