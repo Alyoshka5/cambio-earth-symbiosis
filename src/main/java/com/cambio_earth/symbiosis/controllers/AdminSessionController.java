74200a1566d7f6a01f33aba3e67c50ae42b00cda
Mitchell Verma <mitchellmv11@gmail.com>
2026-03-09 04:13:43 -0700

    feat: added session details page and backend of deleting a user from an enlisted session

--- src/main/java/com/cambio_earth/symbiosis/controllers/AdminSessionController.java
+++ src/main/java/com/cambio_earth/symbiosis/controllers/AdminSessionController.java
@@ -1,24 +1,32 @@
 package com.cambio_earth.symbiosis.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
-import com.cambio_earth.symbiosis.models.Session;
-import com.cambio_earth.symbiosis.models.SessionRepository;
+import com.cambio_earth.symbiosis.models.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
+
 
 @Controller
 public class AdminSessionController {
 
     @Autowired
     SessionRepository sessionRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ParticipationRepository participationRepository;
 
     // AdminSessionForm.html 
 
@@ -65,10 +73,36 @@ public class AdminSessionController {
 
     // Show session detail page
     @GetMapping("/sessions/{id}")
-    public String getSessionDetails(@PathVariable Long id, Model model) {
+    public String getSessionDetails(@PathVariable Long id, Model model, @AuthenticationPrincipal User currentUser) {
         Session session = sessionRepository.findById(id).orElseThrow();
         model.addAttribute("session", session);
+
+        // Add an attribute for checking if the current logged in user is an admin
+        boolean isAdmin = currentUser != null && currentUser.getRole() == Role.ADMIN;
+        model.addAttribute("isAdmin", isAdmin);
+        System.out.println(">>> currentUser is: " + currentUser);
+        System.out.println(">>> session title is: " + session.getTitle());
+
         return "addDetails";
+    }
+
+    // Remove a user from a session
+    @PostMapping("/remove/{uid}/fromSession/{sid}")
+    public String removeUserFromSession(@PathVariable Long uid, @PathVariable Long sid) {
+        
+        // Get the session and user object
+        Session session = sessionRepository.findById(sid).orElseThrow();
+        User user = userRepository.findById(uid).orElseThrow();
+        
+        // Find the session the desired user is in within the participation table
+        Optional<Participation> participation = participationRepository.findFirstBySessionAndUser(session, user);
+
+        // Delete the session the user is in if found in the participations table
+        if (participation.isPresent()) {
+            participationRepository.delete(participation.get());
+        }
+        
+        return "redirect:/sessions/" + sid;
     }
 
     // Delete (admin only)
--- src/main/java/com/cambio_earth/symbiosis/models/ParticipationRepository.java
+++ src/main/java/com/cambio_earth/symbiosis/models/ParticipationRepository.java
@@ -2,8 +2,12 @@ package com.cambio_earth.symbiosis.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
-
+    List<Participation> findBySession(Session session);
+    List<Participation> findBySessionAndUser(Session session, User user);
+    Optional<Participation> findFirstBySessionAndUser(Session session, User user);
 }
