8173cdd98a07403cf4b23c3b092522fb8fe8f175
Mitchell Verma <mitchellmv11@gmail.com>
2026-03-13 02:34:48 -0700

    feat: add-pasting-verification-code-all-at-once

--- src/main/resources/static/js/verificationCode.js
+++ src/main/resources/static/js/verificationCode.js
@@ -14,3 +14,39 @@ codeInputs.forEach(input => {
         }
     });
 });
+
+
+for (let i = 0; i < codeInputs.length; i++) {
+    codeInputs[i].addEventListener('paste', function(e) {
+
+        // Prevent pasting everything to the first box or only the first box
+        e.preventDefault();
+        codeInputs.forEach(input => input.removeAttribute('maxlength'));
+
+        // Grab pasted content and remove any non-digit values
+        const pasted = (e.clipboardData || window.clipboardData)
+            .getData('text')
+
+        // Fill each box starting from the one that was pasted into
+        for (let j = 0; j < pasted.length; j++) {
+            let currentBox = codeInputs[i + j];
+
+            if (currentBox) {
+                currentBox.value = pasted[j];
+            }
+        }
+
+        let lastFilledIndex = i + pasted.length - 1;
+
+        // Don't paste anything past input box 5
+        if (lastFilledIndex > codeInputs.length - 1) {
+            lastFilledIndex = codeInputs.length - 1;
+        }
+
+        // Move the cursor to input box 5 after filling the boxes
+        codeInputs[lastFilledIndex].focus();
+
+        // Add back the maxlength attribute for input boxes
+        codeInputs.forEach(input => input.setAttribute('maxlength', '1'));
+    });
+}
