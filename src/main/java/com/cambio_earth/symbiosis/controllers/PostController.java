8f497968b930c6e01627ef0fa0b7d4d0284cef26
Mitchell Verma <mitchellmv11@gmail.com>
2026-03-17 19:06:17 -0700

    feat: add repository attributes to post controller

--- src/main/java/com/cambio_earth/symbiosis/controllers/PostController.java
+++ src/main/java/com/cambio_earth/symbiosis/controllers/PostController.java
@@ -2,16 +2,26 @@ package com.cambio_earth.symbiosis.controllers;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cambio_earth.symbiosis.models.PostRepository;
+import com.cambio_earth.symbiosis.models.UserRepository;
+
 
 
 @Controller
 public class PostController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
 
     // Load the add post details form page
     @GetMapping("posts/form")
@@ -22,7 +32,6 @@ public class PostController {
     // Process adding a new post
     @PostMapping("posts/add")
     public String addPost(@RequestParam Map<String, String> inputs, Model model) {
-        //TODO: process adding a post
         
         return "";
     }
