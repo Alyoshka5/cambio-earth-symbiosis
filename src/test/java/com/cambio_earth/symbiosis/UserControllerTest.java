d9563e778184767b861b0794ec1680d34967b58b
Alyoshka <alyoshkamyshko@gmail.com>
2026-03-31 00:50:01 -0700

    test: add profile editing tests

--- src/test/java/com/cambio_earth/symbiosis/UserControllerTest.java
+++ src/test/java/com/cambio_earth/symbiosis/UserControllerTest.java
@@ -8,9 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,6 +21,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 
 import com.cambio_earth.symbiosis.controllers.UserController;
+import com.cambio_earth.symbiosis.dto.LoginUserDto;
+import com.cambio_earth.symbiosis.dto.ProfileDto;
 import com.cambio_earth.symbiosis.models.Post;
 import com.cambio_earth.symbiosis.models.User;
 import com.cambio_earth.symbiosis.models.UserRepository;
@@ -29,7 +34,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
-
     @Mock
     private UserRepository userRepository;
 
@@ -140,5 +144,189 @@ public class UserControllerTest {
         String result = userController.getProfilePage(999L, request, response, model);
 
         assertEquals("redirect:/auth/login", result);
+    }
+
+    @Test
+    void testViewEditProfile_UserNotLoggedIn_RedirectsToLogin() {
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(null);
+
+        String result = userController.getProfileEditForm(request, model);
+
+        assertEquals("redirect:/auth/login", result);
+    }
+
+    @Test
+    void testUpdateProfileFirstName_Success_SavesChanges() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("Macintosh");
+        profileDto.setCurrentPassword("");
+        profileDto.setNewPassword("");
+        profileDto.setConfirmNewPassword("");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Bob");
+        testUser.setLastName("Macintosh");
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("redirect:/profile", result);
+        verify(userRepository).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfileFirstName_FirstNameTooShort_RedirectToForm() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("J");
+        profileDto.setLastName("Macintosh");
+        profileDto.setCurrentPassword("");
+        profileDto.setNewPassword("");
+        profileDto.setConfirmNewPassword("");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Bob");
+        testUser.setLastName("Macintosh");
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("profileEditForm", result);
+        verify(userRepository, never()).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfileLastName_Success_SavesChanges() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("Doe");
+        profileDto.setCurrentPassword("");
+        profileDto.setNewPassword("");
+        profileDto.setConfirmNewPassword("");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Joe");
+        testUser.setLastName("Macintosh");
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("redirect:/profile", result);
+        verify(userRepository).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfileLastName_LastNameTooShort_RedirectToForm() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("D");
+        profileDto.setCurrentPassword("");
+        profileDto.setNewPassword("");
+        profileDto.setConfirmNewPassword("");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Joe");
+        testUser.setLastName("Macintosh");
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("profileEditForm", result);
+        verify(userRepository, never()).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfilePassword_Success_SavesChanges() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("Macintosh");
+        profileDto.setCurrentPassword("currentPassword");
+        profileDto.setNewPassword("newPassword");
+        profileDto.setConfirmNewPassword("newPassword");
+
+        when(passwordEncoder.encode("currentPassword")).thenReturn("encryptedPassword");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Joe");
+        testUser.setLastName("Macintosh");
+        testUser.setPassword(passwordEncoder.encode("currentPassword"));
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("redirect:/profile", result);
+        verify(userRepository).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfilePassword_IncorrectPassword_RedirectToForm() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("Macintosh");
+        profileDto.setCurrentPassword("wrongPassword");
+        profileDto.setNewPassword("newPassword");
+        profileDto.setConfirmNewPassword("newPassword");
+
+        LoginUserDto authenticationDto = new LoginUserDto();
+        authenticationDto.setEmail("joem@gmail.com");
+        authenticationDto.setPassword(profileDto.getCurrentPassword());
+
+        when(passwordEncoder.encode("currentPassword")).thenReturn("encryptedPassword");
+        when(passwordEncoder.encode("wrongPassword")).thenReturn("encryptedWrongPassword");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Joe");
+        testUser.setLastName("Macintosh");
+        testUser.setPassword(passwordEncoder.encode("currentPassword"));
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+        when(authenticationService.authenticate(any(LoginUserDto.class))).thenReturn(null);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("profileEditForm", result);
+        verify(userRepository, never()).save(testUser);
+    }
+
+    @Test
+    void testUpdateProfilePassword_PasswordsDontMatch_RedirectToForm() {
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setFirstName("Joe");
+        profileDto.setLastName("Macintosh");
+        profileDto.setCurrentPassword("currentPassword");
+        profileDto.setNewPassword("newPassword");
+        profileDto.setConfirmNewPassword("newMismatchedPassword");
+
+        LoginUserDto authenticationDto = new LoginUserDto();
+        authenticationDto.setEmail("joem@gmail.com");
+        authenticationDto.setPassword(profileDto.getCurrentPassword());
+
+        when(passwordEncoder.encode("currentPassword")).thenReturn("encryptedPassword");
+
+        testUser = new User();
+        testUser.setEmail("joem@gmail.com");
+        testUser.setFirstName("Joe");
+        testUser.setLastName("Macintosh");
+        testUser.setPassword(passwordEncoder.encode("currentPassword"));
+
+        when(authenticationService.getUserFromRequest(request)).thenReturn(testUser);
+        when(authenticationService.authenticate(any(LoginUserDto.class))).thenReturn(testUser);
+
+        String result = userController.saveProfileInfo(request, model, profileDto);
+
+        assertEquals("profileEditForm", result);
+        verify(userRepository, never()).save(testUser);
     }
 }
