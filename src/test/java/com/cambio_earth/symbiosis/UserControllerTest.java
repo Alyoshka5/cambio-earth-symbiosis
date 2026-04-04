843c7e793e9f7df01e95806c9e2ee8c69d5c89ed
Talha <talha2khan2k@gmail.com>
2026-04-04 12:39:42 -0700

    add user as admin from participants list

--- src/test/java/com/cambio_earth/symbiosis/UserControllerTest.java
+++ src/test/java/com/cambio_earth/symbiosis/UserControllerTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
